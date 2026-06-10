b485af7a7660207d46fa14bb4e5e031d7051fb15
wafula <wafulaat@gmail.com>
2026-06-10 12:00:57 +0300

    feat: add v16 configurations

--- rfq_opening_process/hooks.py
+++ rfq_opening_process/hooks.py
@@ -7,7 +7,7 @@ from .rfq_opening_process.doctype.request_for_quotation.request_for_quotation im
 )
 
 app_name = "rfq_opening_process"
-app_title = "Navari RFQ Opening Process"
+app_title = "RFQ Opening Process"
 app_publisher = "Navari Limited"
 app_description = "ERPNext Extensions on Request for Quotation and Supplier Quotation to include Committee Bid Openings"
 app_email = "support@navari.co.ke"
